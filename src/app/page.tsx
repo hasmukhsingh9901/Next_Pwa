8568d1044ef1696c9f5b5e92d596ead82a0603ac
hasmukhsingh990 <hasmukhsingh90@gmail.com>
2024-09-24 14:43:09 +0530

    Antd added and Implented Collapse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,42 @@
 "use client"
 import React from 'react'
+import type { CollapseProps } from 'antd';
+import { Collapse } from 'antd';
+
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
+
+const items: CollapseProps['items'] = [
+  {
+    key: '1',
+    label: 'This is panel header with arrow icon',
+    children: <p>{text}</p>,
+  },
+  {
+    key: '2',
+    label: 'This is panel header with no arrow icon',
+    children: <p>{text}</p>,
+  },
+];
 
 const Home = () => {
+  const onChange = (key: string | string[]) => {
+    console.log(key);
+  };
   return (
-    <div>Home</div>
+    <div className='w-screen h-screen bg-background'>
+      <Collapse defaultActiveKey={['1']} onChange={onChange} items={items} />
+    </div>
   )
 }
 
 export default Home
+
+
+
+
+
+
